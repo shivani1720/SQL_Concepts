a1adc9dfe8ee07bb0da2f0c34bb1be91805fde6b
shivani1720 <60249782+shivani1720@users.noreply.github.com>
2026-04-18 14:26:27 -0400

    Update world_cup.sql

--- world_cup.sql
+++ world_cup.sql
@@ -8,3 +8,26 @@ INSERT INTO icc_world_cup values('India', 'SL', 'India');
 INSERT INTO icc_world_cup values('West Indies', 'SA', 'SA');
 INSERT INTO icc_world_cup values('Pakistan', 'India', 'India');
 INSERT INTO icc_world_cup values('Australia', 'India', 'Australia');
+
+
+QUESTION: I want the output as Team_Name, No of matches_played, No_of_wins, No_of_losses
+
+SELECT team_name, count(1) as no_of_matches_played, SUM(win_flag) as no_of_matches_won, 
+count(1) - SUM(win_flag) as no_of_losses
+FROM
+(  
+SELECT Team_1 as team_name, 
+  CASE 
+    WHEN Team_1 = Winner then 1 
+    ELSE 0 
+  END AS win_flag
+FROM icc_world_cup
+UNION ALL
+SELECT Team_2 as team_name,
+  CASE
+    WHEN Team_2 = Winner then 1
+    ELSE 0
+END AS win_flag
+FROM icc_world_cup ) A
+GROUP BY team_name
+ORDER BY no_of_matches_won DESC
