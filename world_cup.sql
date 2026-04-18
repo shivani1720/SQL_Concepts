75bb301909fbd00c5e66227e4128716986558742
shivani1720 <60249782+shivani1720@users.noreply.github.com>
2026-04-18 14:13:41 -0400

    Update world_cup.sql

--- world_cup.sql
+++ world_cup.sql
@@ -1,1 +1,10 @@
+CREATE TABLE icc_world_cup (
+  Team_1 VARCHAR(20),
+  Team_2 VARCHAR(20),
+  Winner VARCHAR(20)
+  );
 
+INSERT INTO icc_world_cup values('India', 'SL', 'India');
+INSERT INTO icc_world_cup values('West Indies', 'SA', 'SA');
+INSERT INTO icc_world_cup values('Pakistan', 'India', 'India');
+INSERT INTO icc_world_cup values('Australia', 'India', 'Australia');
